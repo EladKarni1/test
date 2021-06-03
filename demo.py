10d32099cf4d835c1d8b7f1f2bbc6999ad95f410
EladKarni1 <81392654+EladKarni1@users.noreply.github.com>
2021-06-03 12:59:30 +0300

    Update demo.py

--- demo.py
+++ demo.py
@@ -9,18 +9,18 @@ issues = []
 
 for i in range(len(lines)):
     if lines[i].startswith("T")://if starts with "T"
-        if lines[i].split(':')[1] not in titles://split and take location 1 and check if its not in the list "titles" already
-            titles.append(lines[i].split(':')[1])//if its not, add it to titles list
+        if lines[i].split(':')[1] not in titles:    //split and take location 1 and check if its not in the list "titles" already
+            titles.append(lines[i].split(':')[1])   //if its not, add it to titles list
     if lines[i].startswith("Iss"):
-        if lines[i].split(':')[1] not in issues: ////split and take location 1 and check if it's not in the list "issues" already
-            issues.append(lines[i].split(':')[1])////if its not,add it to issues list
+        if lines[i].split(':')[1] not in issues:    //split and take location 1 and check if it's not in the list "issues" already
+            issues.append(lines[i].split(':')[1])   //if its not,add it to issues list
     if lines[i].startswith("[ NOTE_MISSING ]"):
               if lines[i+2].startswith("Owner: chdauto") | lines[i+2].startswith("Owner: syspdbuild") | lines[i+2].startswith("Owner: syspdbuild2"):
                continue
               else:
                f2.write(lines[i])
                f2.write("@"+lines[i+2].replace("Owner: ", "").replace("\n", "")+",Please add git notes to this commit!\n")
-for index, title in enumerate(titles)://give me the location of titles and title in titles
+for index, title in enumerate(titles):    //give me the location of titles and title in titles
     f2.write('[' + issues[index].replace(' ', '').replace("\n", "") + ']' + title)
 
 
